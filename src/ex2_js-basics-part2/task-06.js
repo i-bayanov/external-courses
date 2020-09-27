bec746ad39d54b9a877926cfcd630e006b3abc4d
Ilya Bayanov <i.p.bayanov@gmail.com>
2020-09-28 01:48:19 +0300

    ex2_js-basics-part2 is done

---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,23 @@
+function checkPrimeNumber (a) {
+  if (a === 0) {
+    return (`${a} - это ноль`);
+  }
+
+  if (a === 1) {
+    return (`${a} - это один, не простое и не составное число`);
+  }
+
+  if (a > 1000) {
+    return ('Данные неверны');
+  }
+
+  for (let i = 2; i < (a/3); i++) {
+    if (a % i === 0) {
+      return (`Число ${a} - составное число`);
+    }
+  }
+
+  return (`Число ${a} - простое число`);
+}
+
+module.exports = checkPrimeNumber;
