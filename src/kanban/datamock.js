be9dfcaa9d4757d213c9ba1b17fcbf5da5f4fea8
Ilya Bayanov <i.p.bayanov@gmail.com>
2020-11-16 09:06:11 +0300

    kanban task lists some additions

---
+++ src/kanban/datamock.js
@@ -0,0 +1,82 @@
+const dataMock = [
+  {
+    title: 'backlog',
+    issues: [
+      {
+        id: 'task1',
+        name: 'Login page – performance issues',
+      },
+      {
+        id: 'task2',
+        name: 'Sprint bugfix',
+      },
+    ],
+  },
+  {
+    title: 'ready',
+    issues: [
+      {
+        id: 'task1',
+        name: 'Shop page – performance issues',
+      },
+      {
+        id: 'task2',
+        name: 'Checkout bugfix',
+      },
+      {
+        id: 'task3',
+        name: 'Shop bug1',
+      },
+      {
+        id: 'task4',
+        name: 'Shop bug2',
+      },
+      {
+        id: 'task5',
+        name: 'Shop bug3',
+      },
+      {
+        id: 'task6',
+        name: 'Shop bug4',
+      },
+      {
+        id: 'task7',
+        name: 'Shop bug5',
+      },
+      {
+        id: 'task8',
+        name: 'Shop bug6',
+      },
+      {
+        id: 'task9',
+        name: 'Shop page – performance issues',
+      },
+    ],
+  },
+  {
+    title: 'inProgress',
+    issues: [
+      {
+        id: 'task1',
+        name: 'User page – performance issues',
+      },
+      {
+        id: 'task2',
+        name: 'Auth bugfix',
+      },
+    ],
+  },
+  {
+    title: 'finished',
+    issues: [
+      {
+        id: 'task1',
+        name: 'Main page – performance issues',
+      },
+      {
+        id: 'task2',
+        name: 'Main page bugfix',
+      },
+    ],
+  },
+]
--- src/kanban/tasks.js
+++ src/kanban/tasks.js
@@ -7,17 +7,28 @@ const inProgressList = inProgress.querySelector('.listOfTasks');
 const finished = document.querySelector('.taskList.finished');
 const finishedList = finished.querySelector('.listOfTasks');
 
-const backlogAdd = backlog.querySelector('.addCard');
+const addTask = document.getElementsByClassName('addCard');
 const newTaskInput = document.createElement('input');
 
-const addCard = () => {
-  newTaskInput.type = 'text';
-  newTaskInput.name = 'description';
-  newTaskInput.placeholder = 'Describe new task';
-  newTaskInput.classList.add('task');
-  newTaskInput.style = 'display: block; width: calc(100% - 7px);';
-  backlogList.appendChild(newTaskInput);
-  newTaskInput.focus();
+const addCard = (event) => {
+  if (event.currentTarget.parentNode === backlog) {
+    newTaskInput.type = 'text';
+    newTaskInput.name = 'description';
+    newTaskInput.placeholder = 'Describe new task';
+    newTaskInput.classList.add('task');
+    newTaskInput.style = 'display: block; width: calc(100% - 7px);';
+    backlogList.appendChild(newTaskInput);
+    newTaskInput.focus();
+  } else {
+    const prevTasks = event.currentTarget.parentNode.previousElementSibling.getElementsByClassName('task');
+    prevTasksList = document.createElement('div');
+    prevTasksList.classList.add('prevTasksList');
+    prevTasksList.appendChild(document.createElement('ul'));
+    for (let i = 0; i < prevTasks.length; i++) {
+      prevTasksList.firstElementChild.appendChild(prevTasks[i].cloneNode(true));
+    }
+    event.currentTarget.parentNode.appendChild(prevTasksList);
+  }
 }
 
 const saveTask = () => {
@@ -33,5 +44,8 @@ const saveTask = () => {
   newTaskInput.value = '';
 }
 
-backlogAdd.addEventListener('click', addCard);
+for (let i = 0; i < addTask.length; i++) {
+  addTask[i].addEventListener('click', addCard);
+}
+
 newTaskInput.addEventListener('blur', saveTask);
